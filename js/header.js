1b419fc93bc5a5a126bdcbfb9efb76e889dd2c9a
Rafael A Boscan <51136553+rboscan@users.noreply.github.com>
2020-09-14 22:37:46 -0400

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,15 +1,27 @@
 function checkScroll() {
     var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
 
-    if ($(window).scrollTop() > startY) {
+    if (($(window).scrollTop() > startY)) {
         $('.navbar').addClass("scrolled");
     } else {
         $('.navbar').removeClass("scrolled");
-    }
-}
+    };
+};
 
 if ($('.navbar').length > 0) {
     $(window).on("scroll load resize", function () {
         checkScroll();
     });
 };
+
+function checkButton() {
+    if (!$(".navbar").hasClass("scrolled-2")) {
+        $(".navbar").addClass("scrolled-2");
+    } else {
+        $(".navbar").removeClass("scrolled-2");
+    }
+}
+
+if ($("button.navbar-toggler").click(function () {
+    checkButton();
+}));
